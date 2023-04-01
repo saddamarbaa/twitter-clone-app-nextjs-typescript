61a1d50763db62715968bbd12d8cfb2ec1e711ef
saddamarbaa <saddamarbaas@gamil.com>
2023-04-01 09:51:11 +0700

    chore:  enable next 13 experimental feature

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export default function Home() {
+  return <div className='bg-red-500'>ok</div>;
+}
--- src/pages/index.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <p className="text-red-500">
-      Hello world!
-    </p>
-  )
-}
